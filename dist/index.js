5318d20c2ae0d6dbcbc82acb570c2c1a5607a58c
mikegetz <getzmd02@gmail.com>
2023-01-28 16:02:38 -0700

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,20 @@
+import { execa } from 'execa';
+import { Command } from 'commander';
+const program = new Command();
+const start = async () => {
+    const { stdout } = await execa('echo', ['Hello World!']);
+    console.log(stdout);
+};
+program
+    .name('mp4-edit-metadata')
+    .description('CLI to edit mp4 metadata')
+    .version('1.0.0');
+program.command('edit')
+    .description('Edit an mp4 file')
+    .argument('<file>', 'file to edit')
+    .action((str, options) => {
+    start();
+    console.log(`str: ${str}, options: ${JSON.stringify(options)}`);
+});
+program.parse();
+//# sourceMappingURL=index.js.map
