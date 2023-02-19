6e35e5abe79985c186233e5a5d0d0cd157468532
mikegetz <getzmd02@gmail.com>
2023-02-19 14:53:32 -0700

    refactored function name more inline with mpeg, pull command into seperate const

--- src/operations/view/view.ts
+++ src/operations/view/view.ts
@@ -1,10 +1,11 @@
 import { MetaData, } from 'src/types/metadata';
 import { exec, } from '../../exec';
 
-const viewMp4FileMetadata = async (file: string): Promise<MetaData> => {
-  const result: string = await exec(`ffprobe -loglevel 0 -print_format json -show_entries 'format_tags' ${file}`);
+const viewMpegFileMetadata = async (file: string): Promise<MetaData> => {
+  const command = `ffprobe -loglevel 0 -print_format json -show_entries 'format_tags' ${file}`;
+  const result: string = await exec(command);
   const metatdata: MetaData = JSON.parse(result) as MetaData;
   return metatdata;
 };
 
-export default viewMp4FileMetadata;
+export default viewMpegFileMetadata;
