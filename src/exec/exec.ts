c901ebd9060d53783aa7d0a81efe3194ccf6ecf2
mikegetz <getzmd02@gmail.com>
2023-02-18 18:08:53 -0700

    promise handling better to surface shell error

--- src/exec/exec.ts
+++ src/exec/exec.ts
@@ -1,14 +1,14 @@
 import * as child from 'child_process';
 
 const exec = (command: string): Promise<string> => {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     child.exec(command, (error, stdout, stderr) => {
       if (error?.stack) {
-        resolve(error.stack);
+        reject(error.stack);
         return;
       }
       if (stderr) {
-        resolve(stderr);
+        reject(stderr);
         return;
       }
       resolve(stdout);
